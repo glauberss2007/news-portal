599b38d8b9d750f4f2741865cac70ff9513c039b
glauberss2007 <glauberss2007@gmail.com>
2021-02-04 18:17:16 -0300

    manual route implementation

--- news.js
+++ news.js
@@ -1,11 +1,29 @@
 //console.log('Creating a nodjs news portal')
+
+//Importing lib
 var http = require('http');
 
+//Creating server
 var server = http.createServer(function(req,res){
-	res.end("<html><body>News portal</body></html>");
+
+	//Redirect diferent routes based on url req
+	var category = req.url;
+	if(category == '/tecnology'){
+		res.end("<html><body>Tecnology News</body></html>");
+	}else if(category == '/fashion'){
+		res.end("<html><body>Fashion News</body></html>");
+	}else if(category == '/beauty'){
+		res.end("<html><body>Beauty News</body></html>");
+	}else {
+		res.end('<html><body>General News</body></html>');
+	}
+	
+//	res.end("<html><body>News portal</body></html>");
 });
 
+//Start listen on port
 server.listen(3000);
+
 
 
 
