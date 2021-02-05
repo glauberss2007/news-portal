25be93dba06c971f42bef55b739b060c3b692911
glauberss2007 <glauberss2007@gmail.com>
2021-02-05 09:37:08 -0300

    syntax bug fix

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 var express = require('express');
 var app = express();
 
+// using ejs lib to dynamic html use thrught ejs file
+app.set('view engine', 'ejs');
+
+// dinamic html view
 app.get('/', function(req,res){
-	res.send("<html><body>News Portal</body></html>")
+	res.send("<html><body>News Portal</body></html>");
 });
 
-app.get('/tecnology', function(req,res){
-	res.send("<html><body>Technology News</body></html>")
+app.get('/technology', function(req, res){
+	res.render('section/technology');
 });
 
 app.listen(3000, function(){
